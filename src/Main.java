3c7d35dd9cd0f57cee8af83c14a0e75463ee6f4b
FocusFine89 <niivanov02@gmail.com>
2024-05-20 14:58:31 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int result=moltiplicatore(2,4);
+        System.out.println(result);
+    }
+
+    public static int moltiplicatore(int num1, int num2){
+        return num1*num2;
+    }
+}
